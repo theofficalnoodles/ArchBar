1857b1b1b881f06b8bb00548221aeb637dd914f1
theofficalnoodles <doodi.jamil1@gmail.com>
2026-01-01 13:40:44 +0300

    Update ArchBarContainment.h

--- ArchBarContainment.h
+++ ArchBarContainment.h
@@ -1,1 +1,17 @@
+#ifndef ARCHBARCONTAINMENT_H
+#define ARCHBARCONTAINMENT_H
 
+#include <Plasma/Containment>
+
+class ArchBarContainment : public Plasma::Containment
+{
+    Q_OBJECT
+    
+public:
+    ArchBarContainment(QObject *parent, const QVariantList &args);
+    ~ArchBarContainment() override;
+    
+    void init() override;
+};
+
+#endif // ARCHBARCONTAINMENT_H
